3e266b6427e7a8ffd564c12097821a3d10c24427
TheHiberNate <nathan@gawargy.com>
2022-11-19 12:30:50 -0500

    Updates

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,7 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnImpactScore:
-
+                startActivity(new Intent(getApplicationContext(), ProductScore.class));
         }
     }
 }
