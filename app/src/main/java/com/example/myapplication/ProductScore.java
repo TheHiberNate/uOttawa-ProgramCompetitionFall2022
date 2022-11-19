4d0b381a6b065dbdcdf829c2f44650dcd11acd11
TheHiberNate <nathan@gawargy.com>
2022-11-19 13:51:27 -0500

    Update ProductScore.java

--- app/src/main/java/com/example/myapplication/ProductScore.java
+++ app/src/main/java/com/example/myapplication/ProductScore.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,11 +10,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.w3c.dom.Text;
 
 public class ProductScore extends AppCompatActivity implements View.OnClickListener {
     private Button calculate;
     private TextView score;
+    private DatabaseReference productReference;
+    private EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,8 @@ public class ProductScore extends AppCompatActivity implements View.OnClickListe
         calculate = (Button) findViewById(R.id.button);
         calculate.setOnClickListener(this);
 
+        name = (EditText) findViewById(R.id.productName);
+
     }
 
     @Override
@@ -31,8 +42,31 @@ public class ProductScore extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.button:
                 // method to calculate here
+                String id = String.valueOf(name.getText());
+                productReference = FirebaseDatabase.getInstance().getReference("Products").child(id);
+                calculate();
                 score.setText(answer);
                 break;
         }
     }
+
+    private int calculate() {
+        productReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Integer distance = (Integer) snapshot.child("distance").getValue();
+                String type = (String) snapshot.child("transportationMeans").getValue();
+                
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return
+    }
+
+
 }
